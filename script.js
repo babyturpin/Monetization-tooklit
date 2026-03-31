7bf7d92a40731af318daa0fd1a06e7dd065255b6
babyturpin <babyturpin9@gmail.com>
2026-04-01 02:47:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function generate() {
+  let topic = document.getElementById("topic").value;
 
+  let result = "Hook: You won’t believe this about " + topic +
+               "\n\nContent: Here’s why it matters..." +
+               "\n\nCTA: Follow for more!";
+
+  document.getElementById("output").innerText = result;
+}
